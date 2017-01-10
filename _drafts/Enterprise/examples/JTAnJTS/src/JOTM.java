67e07f73048bcdeb23fcfe0aed8bd1ae9400f83d
Jianwei Liu <jianliuwei@users.noreply.github.com>
2017-01-10 23:44:42 +0800

    Update JOTM.java

--- _drafts/Enterprise/examples/JTAnJTS/src/JOTM.java
+++ _drafts/Enterprise/examples/JTAnJTS/src/JOTM.java
@@ -1,39 +1,50 @@
-
-
+import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
+import javax.naming.NamingException;
+import javax.sql.XAConnection;
+import javax.sql.XADataSource;
 import javax.transaction.UserTransaction;
 
+import org.enhydra.jdbc.standard.StandardXADataSource;
+import org.objectweb.jotm.Jotm;
+import org.objectweb.transaction.jta.TMService;
+
 public class JOTM extends JTADemo {
-	
+
+	private TMService jotm;
+
+	public JOTM() throws NamingException {
+		jotm = new Jotm(true, false);
+	}
+
 	public static void main(String[] args) throws Exception {
 		new JOTM().demoJTA();
 	}
 
 	@Override
 	protected UserTransaction getUserTransaction() throws Exception {
-		return null;
+		return jotm.getUserTransaction();
 	}
 
 	@Override
-	protected void transactA(DataSource ds) throws SQLException {
-
+	protected Connection getConnectionA() throws SQLException {
+		XADataSource xads = new StandardXADataSource();
+		((StandardXADataSource) xads).setDriverName("oracle.jdbc.driver.OracleDriver");
+		((StandardXADataSource) xads).setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		((StandardXADataSource) xads).setTransactionManager(jotm.getTransactionManager());
+		XAConnection xaconn = xads.getXAConnection("tbeos", "tbeos");
+		return xaconn.getConnection();
 	}
 
 	@Override
-	protected void transactB(DataSource ds) throws SQLException {
-
-	}
-
-	@Override
-	protected DataSource getDataSourceA() {
-		return null;
-	}
-
-	@Override
-	protected DataSource getDataSourceB() {
-		return null;
+	protected Connection getConnectionB() throws SQLException {
+		XADataSource xads = new StandardXADataSource();
+		((StandardXADataSource) xads).setDriverName("oracle.jdbc.driver.OracleDriver");
+		((StandardXADataSource) xads).setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		((StandardXADataSource) xads).setTransactionManager(jotm.getTransactionManager());
+		XAConnection xaconn = xads.getXAConnection("tbeos", "tbeos");
+		return xaconn.getConnection();
 	}
 
 }
