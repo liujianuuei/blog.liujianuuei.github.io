470b1cc096c69dff75ed8d227f33155570a91730
Jianwei Liu <jianliuwei@users.noreply.github.com>
2017-01-10 23:35:33 +0800

    Update JTADemo.java

--- _drafts/Enterprise/examples/JTAnJTS/src/JTADemo.java
+++ _drafts/Enterprise/examples/JTAnJTS/src/JTADemo.java
@@ -1,6 +1,6 @@
-
-
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 import javax.transaction.UserTransaction;
@@ -23,11 +23,27 @@ public abstract class JTADemo {
         }
     }
 
+    protected void transactA(DataSource ds) throws SQLException {
+        Connection conn = ds.getConnection();
+
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("insert into buyer_private_identity(id, buyer_org, supplier_org, private_id) values(buyer_private_identity_seq.nextval, 109, 110, 'jta3')"); // Can be any DML.
+        stmt.close();
+
+        conn.close();
+    }
+
+    protected void transactB(DataSource ds) throws SQLException {
+        Connection conn = ds.getConnection();
+
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("INSERT INTO dept VALUES (3, 'BSD', 'LONDON')"); // SQL: create table(id int, name varchar(50), location varchar(50)); Can be any DML.
+        stmt.close();
+
+        conn.close();
+    }
+
     protected abstract UserTransaction getUserTransaction() throws Exception;
-
-    protected abstract void transactA(DataSource ds) throws SQLException;
-
-    protected abstract void transactB(DataSource ds) throws SQLException;
 
     protected abstract DataSource getDataSourceA();
 
